cc5f208df9944effdfffc03adcf4fbf091de6b80
Taylor Short <treddshort@gmail.com>
2018-06-29 07:11:08 -0700

    add bootstrap classes

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-    return <li>Video</li>
+const VideoListItem = ({video}) => {
+    const imageUrl = video.snippet.thumbnails.default.url;
+    // console.log(video);
+    
+    return (
+    <li className="list-grou-item">
+        <div className="video-list media">
+            <div className="media-left">
+                <img className="media-object" />
+            </div>
+
+            <div className="media-body">
+              <div className="media-heading"></div>
+            </div>
+         </div>
+    </li>
+  )
 }
 
 export default VideoListItem;
